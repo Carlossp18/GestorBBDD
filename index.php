fd2911f5bd4d8dc71894e3f740f71f31d372f6af
Carlos <csperez97@gmail.com>
2019-01-24 10:22:17 +0100

    Inndex.php permite conectar y seleccionar la base de datos con la que se quiere trabajar

--- index.php
+++ index.php
@@ -1,17 +1,76 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+spl_autoload_register(function($clase) {
+    require "$clase.php";
+});
+
+
+if (isset($_POST['submit'])) {
+    session_start();
+
+    switch ($_POST['submit']) {
+        case "conectar":
+            $host = $_POST['host'];
+            $bd = new BBDD($host);
+            $bases = $bd->selectQuery("show databases");
+            $_SESSION['conectado'] = true;
+            $_SESSION['bases'] = $bases;
+            break;
+        case "acceder":
+            if (isset($_POST['basedatos'])) {
+                $_SESSION['basedatos'] = $_POST['basedatos'];
+            } else {
+                $msj = "Has de seleccionar una base de datos";
+            }
+            var_dump($msj);
+            break;
+        case "desconectar":
+            session_destroy();
+            header("Location:index.php");
+            break;
+    }
+    if (isset($_SESSION['conectado'])) {
+        $bases = $_SESSION['bases'];
+        $conectado = $_SESSION['conectado'];
+    }
+}
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <fieldset>
+            <legend>Datos de conexión</legend>
+            <form action="." method="POST">
+                <label for="host">Host</label>
+                <input type="text" name="host" value="172.17.0.2" id="">
+                <label for="usuario">Usuario</label>
+                <input type="text" name="usuario" value="root" id="">
+                <label for="pass">Password</label>
+                <input type="text" name="pass" value="root" id="">
+                <input type="submit" value="<?php echo ($_SESSION['conectado'] == true) ? "desconectar" : "conectar" ?>" name="submit">
+            </form>
+        </fieldset>
         <?php
-        // put your code here
-        ?>
+        if ($conectado):
+            if (isset($msj))
+                echo "<h3>$msj</h3>";
+            ?>
+            <fieldset>
+                <legend>Bases disponibles</legend>
+                <form action="index.php" method="post">
+                    <?php
+                    foreach ($bases as $base) {
+                        foreach ($base as $b) {
+                            echo "<input type=radio value=$b name=basedatos>";
+                            echo "<label for=basedatos>$b</label><br />";
+                        }
+                    }
+                    ?>
+                    <input type="submit" name="submit" value="acceder">
+                </form>
+            </fieldset>
+        <?php endif; ?>
     </body>
 </html>
